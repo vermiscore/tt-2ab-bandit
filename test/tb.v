351d2712f9a16f05254171b34d637c30f4d0d019
vermiscore <249220516+vermiscore@users.noreply.github.com>
2026-05-15 01:57:05 -0700

    fix: update tb.v to use tt_um_2ab_bandit

--- test/tb.v
+++ test/tb.v
@@ -23,8 +23,8 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  // Replace tt_um_2ab_bandit with your module name:
+  tt_um_2ab_bandit user_project (
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
